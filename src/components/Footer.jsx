9b76b303f0f821e39b9873ae789180d8158c3899
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-19 21:38:55 +0000

    mejorado aspecto visual de página principal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
+import "../styles/footer.css"
+
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center bg-dark">
 		<p>
 			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced-project">template documentation</a> <i className="fa-solid fa-file"></i> for help.
 		</p>
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -7,15 +7,15 @@ export const SingleCard = ({name, item1, item2, item3, imglink}) => (
       src={imglink}
       className="card-img-top"
     ></img>
-    <div className="card-body">
-      <h5 className="card-title m-0">{name}</h5>
+    <div className="card-body py-2">
+      <h5 className="card-title m-0 text-center">{name}</h5>
     </div>
-    <ul className="list-group list-group-flush text-start">
-      <li className="list-group-item">{item1}</li>
-      <li className="list-group-item">{item2}</li>
-      <li className="list-group-item">{item3}</li>
+    <ul className="list-group list-group-flush text-start p">
+      <li className="list-group-item py-1">{item1}</li>
+      <li className="list-group-item py-1">{item2}</li>
+      <li className="list-group-item py-1">{item3}</li>
     </ul>
-    <div className="card-body d-flex justify-content-between align-items-center">
+    <div className="card-body d-flex justify-content-between align-items-center p-2">
       <button type="button" className="btn btn-outline-info">
         Learn more!
       </button>
