d4fd33557a8c113ca7df2eea204242309b784bb1
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-19 16:29:12 +0000

    creado botón dropdown para favoritos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,47 @@
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
+import { FaHeart } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
 
 export const Navbar = () => {
-
-	return (
-		<nav className="navbar fixed-top navbar-dark bg-dark">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">
-						<img src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254" className="logo"></img>
-					</span>
-				</Link>
-				<div className="ml-auto">
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar fixed-top navbar-dark bg-dark">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">
+            <img
+              src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254"
+              className="logo"
+            ></img>
+          </span>
+        </Link>
+        <div className="ml-auto">
+          <div class="dropdown">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <div className="favorites-icon-wrapper">
+                <FaHeart className="icon-favorites" />
+                <span className="favorites-count">2</span>
+              </div>
+              <strong>Favorites</strong>
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <span>Action</span>
+                    <MdDeleteOutline className="icon-delete" />
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
