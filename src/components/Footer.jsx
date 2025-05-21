71ef752471b42c32d313d66df702a1a05277718e
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-21 00:03:29 +0000

    se ajustan detalles de diseño en details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import "../styles/footer.css"
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center bg-dark">
+	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { fetchWithCache } from "../services/SwapiTech.jsx";
+import "../styles/detail.css"
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { AppConfig } from "../config/config.js";
 
@@ -12,25 +11,23 @@ export const Detail = () => {
   const fields = AppConfig.fieldsToShowInDetails[endpoint];
 
   return (
-    <div className="w-75 container bg-dark p-5">
+    <div className="w-75 container bg-dark p-5 ">
       <div className="row">
         <div className="col-6">
           <img src={`${images[id - 1]}`} className="card-img-top"></img>
         </div>
-        <div className="col-6">
-          <h5 className="text-light">{item.properties.name}</h5>
+        <div className="col-6 border-start border-custom">
+          <h3 className="text-light">{item.properties.name}</h3>
           <p className="text-light">{item.description}</p>
         </div>
       </div>
-      <div className="card-body">
-        <div className="row">
-          {fields.slice(0, 7).map((field) => (
-            <div className="col" key={field.key}>
-              <h5 className="text-light">{field.label}</h5>
-              <p className="text-light">{item.properties[field.key]}</p>
-            </div>
-          ))}
-        </div>
+      <div className="row pt-2 mt-4 border-top border-custom">
+        {fields.slice(0, 7).map((field) => (
+          <div className="col border-start border-custom" key={field.key}>
+            <h5 className="text-light">{field.label}</h5>
+            <p className="text-light">{item.properties[field.key]}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
